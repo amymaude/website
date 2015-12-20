c48d6016a596748bffc0d23b307268f382a6ab15
Amy Bertken <amy.bertken@gmail.com>
2015-12-20 15:25:57 -0700

    update routes and nav styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rollin\' Bones' });
+});
+
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Contact'});
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'About' });
+});
+
+router.get('/menu', function(req, res, next){
+  res.render('menu', {title: 'Menu'});
+});
+
+router.get('/catering', function(req, res, next){
+  res.render('catering', {title: 'Catering'});
 });
 
 module.exports = router;
