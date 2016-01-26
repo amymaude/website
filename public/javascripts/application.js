d58e264ba7aaac08b823c22b7f463d93d4c1f52e
Amy Bertken <amy.bertken@gmail.com>
2016-01-26 12:51:28 -0800

    update index styling and convert to single page app

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,17 +18,23 @@ $(document).ready(function(done){
     // jsonpCallback: "jsonpcallback",
     url: "https://api.instagram.com/v1/users/2228535302/media/recent?access_token=" + token + "&count=5&callback=callbackFunction",
     success: function(data) {
-      images = data.data;
-      console.log(images)
-      $(".announcements span").append(images[0].caption.text)
+      images=data.data
+      var time = moment(images[0].created_time*1000).local().format("dddd, MMMM Do, YYYY")
       var backgroundImage = images[0].images.standard_resolution.url
-
+      console.log(images[0].caption)
+      $(".alert-info").append(time + ": "+ images[0].caption.text)
       $(".item img").first().attr("src", backgroundImage)
       getEmbedCode(images, 0);
     }
+  })
+  $(".navbar-toggle").on("click", function(event){
+    event.preventDefault();
+    $('.navbar-left').css("padding-left", "0");
+    $('.navbar-right').css("padding-right", "0");
   })
 })
 
 $(document).ajaxComplete(function(){
   $('.insta-feed').append(mostRecent);
+  setTimeout(function(){$('iframe').css({"max-width": "500", "margin-right":"auto", "margin-left": "auto"})}, 500);
 })
--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rollin\' Bones', data: token});
 });
 
-router.get('/admin', function(req, res){
-  res.render('signin', {title: 'Admin Sign In'})
-})
-
-router.route('/contact').get(function(req, res, next){
-  res.render('contact', {title: 'Contact'});
-})
-
-router.post('/contact', function (req, res) {
+router.post('/', function (req, res) {
   var mailOpts, smtpTrans;
   smtpTrans = nodemailer.createTransport('SMTP', {
     service: 'gmail',
@@ -27,26 +19,36 @@ router.post('/contact', function (req, res) {
       pass: process.env.EMAIL_PWORD
     }
   });
-
   mailOpts = {
     from: req.body.name +' <'+ req.body.email +'>',
-    to: process.env.EMAIL,
+    to: "amy.bertken@gmail.com",
     subject: 'Website Contact Form',
-    text: req.body.message
+    text: "from: " + req.body.name +", "+  req.body.email +"\nmessage: " + req.body.message
   };
 
   console.log(mailOpts);
   smtpTrans.sendMail(mailOpts, function (error, response) {
     if (error) {
       console.log(error)
-      res.render('contact', { msg: 'Error occured, message not sent.', err: true, page: 'contact' })
+      res.render('index', { msg: 'Error occured, message not sent.', err: true, page: 'index' })
     }
     //Yay!! Email sent
     else {
-        res.render('contact', { msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+        res.render('index', { msg: 'Message sent! Thank you.', err: false, page: 'index' })
     }
   });
 });
+
+
+
+router.get('/admin', function(req, res){
+  res.render('signin', {title: 'Admin Sign In'})
+})
+
+router.route('/contact').get(function(req, res, next){
+  res.render('contact', {title: 'Contact'});
+})
+
 
 router.get('/about', function(req, res, next){
   res.render('about', {title: 'About' });
