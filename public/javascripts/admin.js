f141c615b0e57b932814da1be43286df28ea195b
Amy Bertken <amy.bertken@gmail.com>
2016-05-17 19:06:01 -0600

    add editable faqs

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -6,7 +6,9 @@ $(document).on('click', '#submitAbout', function(e){saveAbout()});
 $(document).on('click', '#submitEvents', function(e){e.preventDefault(); saveEvents()});
 $(document).on('click', '#addEvent', function(e){addEvent()});
 $(document).on('click', '.delete-event', function(e){var event = $(this).attr('id'); deleteEvent(event)})
-
+$(document).on('click', '#addFaq', function(e){addFaq()});
+$(document).on('click', '.delete-faq', function(e){var faq = $(this).attr('id'); deleteFaq(faq)})
+$(document).on('click', '#submitFaqs', function(e){e.preventDefault(); saveFaqs()});
 var saveAbout = function(){
 
   var title = $('#aboutTitle').val();
@@ -35,6 +37,23 @@ var saveEvents = function(){
 
 }
 
+var saveFaqs = function(){
+  $('.faq-form-group').each(function(i){
+    var id = $(this).find('#faqQuestion').val();
+    if(id){
+      id = id.replace(/\s+/g, '-').replace(/[?]/g, "").toLowerCase();
+
+      var faqRef = new Firebase('https://crackling-fire-6610.firebaseio.com/SiteText/FAQs/' + id);
+      var question = $(this).find('#faqQuestion').val();
+      var answer = $(this).find('#faqAnswer').val();
+      faqRef.update({question: question, answer: answer})
+    }else{
+      alert('Updated or added faq was invalid')
+    }
+  })
+
+}
+
 
 var deleteEvent= function(event){
   var eventRef = new Firebase('https://crackling-fire-6610.firebaseio.com/SiteText/Events/'+event);
@@ -47,6 +66,45 @@ var deleteEvent= function(event){
   }
   return false;
 }
+
+var deleteFaq= function(faq){
+  var faqRef = new Firebase('https://crackling-fire-6610.firebaseio.com/SiteText/FAQs/'+faq);
+  if (confirm("Are you sure you want to delete this faq?")){
+    if (faq==="new"){
+      $('.faq-form-group#new').remove();
+    }else{
+      faqRef.remove()
+    }
+  }
+  return false;
+}
+var addFaq = function(){
+  if($('.faq-form-group').last().attr('id') !=="new"){
+  $('.faq-form-group').last().append(
+    "<div class='faq-form-group' id='new'>\
+      <div class='form-group'>\
+        <label for='faqQuestion' class='col-sm-2 control-label'>Question</label>\
+        <div class='input-group'>\
+          <div class='col-sm-8'>\
+            <input type='text' id='faqQuestion' class='form-control' value=''>\
+          </div>\
+          <span class='input-group-btn'>\
+            <button type='button' class='btn btn-danger delete-faq' id='new'>Delete this FAQ</button>\
+          </span>\
+        </div>\
+      </div>\
+      <div class='form-group'>\
+        <label for='faqAnswer' class='col-sm-2 control-label'> Answer</label>\
+        <div class='col-md-8'>\
+          <textarea class='form-control col-sm-6' id='faqAnswer' rows='5'></textarea>\
+        </div>\
+      </div>\
+    </div>")
+  }else{
+      alert("You already created a new faq! Save your changes, delete the new faq, or hit the refresh button to reset before creating another new event")
+    }
+}
+
 var addEvent = function(){
   if($('.event-form-group').last().attr('id') !=="new"){
   $('.event-form-group').last().append(
@@ -90,12 +148,13 @@ var addEvent = function(){
         </div>\
       </div>\
     </div>")}else{
-      alert("You already created a new event! Save your changes or hit the refresh button to reset before creating another new event")
+      alert("You already created a new event! Save your changes, delete the new event, or hit the refresh button to reset before creating another new faq")
     }
 }
 
 myFirebaseRef.on("value", function(snapshot) {
   var eventPanels = "<form class='form-horizontal edit-events'>";
+  var faqPanels = "<form class='form-horizontal edit-faqs'>";
   var siteText = snapshot.val().SiteText;
   $("form.edit-about").replaceWith(
     "<form class='form-horizontal edit-about'>\
@@ -108,7 +167,7 @@ myFirebaseRef.on("value", function(snapshot) {
       <div class='form-group'>\
         <label for='aboutText' class='col-sm-2 control-label'> About Section Text</label>\
         <div class='col-md-8'>\
-          <textarea class='form-control col-sm-6' id='aboutText' rows='6'>" + siteText.About.Text + "</textarea>\
+          <textarea class='form-control col-sm-6' id='aboutText' rows='10'>" + siteText.About.Text + "</textarea>\
         </div>\
       </div>\
       <div class='form-group'>\
@@ -122,14 +181,15 @@ myFirebaseRef.on("value", function(snapshot) {
   for(prop in (siteText.Events)){
     var url = (siteText.Events[prop].url) ? siteText.Events[prop].url : '';
     var checked = siteText.Events[prop].inMainPage ? "checked" : '';
-
+    var title = (siteText.Events[prop].title) ? siteText.Events[prop].title : '';
+    var shortDesc = (siteText.Events[prop].shortDesc) ? siteText.Events[prop].shortDesc : ''
    eventPanels = eventPanels +
     "<div class='event-form-group' id='" + prop+"'>\
       <div class='form-group'>\
         <label for='eventTitle' class='col-sm-2 control-label'>Event Title </label>\
         <div class='input-group'>\
           <div class='col-sm-8'>\
-            <input type='text' id='eventTitle' class='form-control' value='" + siteText.Events[prop].title + "'>\
+            <input type='text' id='eventTitle' class='form-control' value='" + title + "'>\
           </div>\
           <span class='input-group-btn'>\
             <button type='button' class='btn btn-danger delete-event' id='"+prop+"'>Delete Event</button>\
@@ -160,7 +220,7 @@ myFirebaseRef.on("value", function(snapshot) {
       <div class='form-group'>\
         <label for='eventText' class='col-sm-2 control-label'> Event Description </label>\
         <div class='col-md-8'>\
-          <textarea class='form-control' id='eventText' rows='4'>" + siteText.Events[prop].shortDesc+"</textarea>\
+          <textarea class='form-control' id='eventText' rows='4'>" + shortDesc+"</textarea>\
         </div>\
       </div>\
     </div>"
@@ -172,19 +232,35 @@ myFirebaseRef.on("value", function(snapshot) {
 </form>";
   $('.edit-events').replaceWith(eventPanels);
 
-})
 
-//   for(prop in (siteText.FAQs)){
-//     $(".faqs-panels").append("<div class='panel panel-default'>\
-//       <div class='panel-heading' role='tab' id='heading" + prop + "'>\
-//        <h4 class='panel-title'>\
-//         <a class='collapsed' role='button' data-toggle='collapse' data-parent='#accordion' href='#"+ prop + "' aria-expanded='false' aria-controls=" + prop + ">" + siteText.FAQs[prop].question + "</a>\
-//        </h4>\
-//       </div>\
-//       <div id=" + prop +" class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading" +prop +"'>\
-//        <div class='panel-body'>" + siteText.FAQs[prop].answer + "\
-//        </div>\
-//       </div>\
-//     </div>");
-//   }
-// })
+  for(prop in (siteText.FAQs)){
+    faqPanels = faqPanels +
+      "<div class='faq-form-group' id='" + prop+"'>\
+        <div class='form-group'>\
+          <label for='faqQuestion' class='col-sm-2 control-label'>Question</label>\
+          <div class='input-group'>\
+            <div class='col-sm-8'>\
+              <input type='text' id='faqQuestion' class='form-control' value='" + siteText.FAQs[prop].question + "'>\
+            </div>\
+            <span class='input-group-btn'>\
+              <button type='button' class='btn btn-danger delete-faq' id='"+prop+"'>Delete this FAQ</button>\
+            </span>\
+          </div>\
+        </div>\
+        <div class='form-group'>\
+          <label for='faqAnswer' class='col-sm-2 control-label'> Answer</label>\
+          <div class='col-md-8'>\
+            <textarea class='form-control col-sm-6' id='faqAnswer' rows='5'>" + siteText.FAQs[prop].answer + "</textarea>\
+          </div>\
+        </div>\
+      </div>"
+  }
+
+  faqPanels += "<div class='form-group' style='padding-top: 20px'><div class='col-sm-offset-2 col-sm-10'><button type='submit' class='btn btn-primary' id='submitFaqs'>Save FAQs Section Changes</button>\
+      <button type='button' class='btn btn-default' id='addFaq'>Add Faq</button>\
+    </div>\
+  </div>\
+</form>";
+  $('.edit-faqs').replaceWith(faqPanels);
+
+})
